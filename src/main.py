12ec0bf32fe73758fabe7b359d78b40ea911bdd7
Simran Tyagi <simran.tyagi@impressico.com>
2021-11-09 15:27:44 +0530

    update

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import importlib
 import time
 import os
 import sys
+from dotenv import dotenv_values, load_dotenv
+import json
 
 if os.path.exists('libs.zip'):
     sys.path.insert(0, 'libs.zip')
@@ -26,6 +28,23 @@ from pyspark.sql import SparkSession
 
 __author__ = 'Impressico'
 
+def load_config():
+    cwd = os.getcwd()
+    path_config_env = os.path.join(cwd, "config", "fetch_from_mongo", "config.env")
+    path_config_json = os.path.join(cwd, "config", "fetch_from_mongo", "config.json")
+
+    load_dotenv(str(path_config_env))
+    MONGO_USER_NAME = os.getenv('MONGO_USER_NAME')
+    MONGO_USER_PWD = os.getenv('MONGO_USER_PWD')
+
+    with open(str(path_config_json)) as f:
+        config = json.load(f)
+
+    config.update({
+        'MONGO_USER_NAME': MONGO_USER_NAME,
+        'MONGO_USER_PWD': MONGO_USER_PWD
+    })
+    return config
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run a PySpark job')
@@ -35,6 +54,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print("Called with arguments: %s" % args)
     # TODO: Load config here.
+    config = load_config()
 
     environment = {
         'PYSPARK_JOB_ARGS': ' '.join(args.job_args) if args.job_args else ''
@@ -50,19 +70,19 @@ if __name__ == '__main__':
 
     # TODO: supply Spark config while creating spark object.
     os.environ.update(environment)
-    spark = SparkSession.\
-                builder.\
-                config("spark.mongodb.input.partitioner", "MongoSamplePartitioner").\
-                config("spark.mongodb.input.partitionerOptions.partitionSizeMB", "128").\
-                config("spark.mongodb.input.partitionerOptions.partitionKey", "brand").\
-                getOrCreate()
+    sparkcon = config.get('spark_conf',None)
+    sparkBuilder = SparkSession.builder
+    for k,v in sparkcon.items():
+        print('key & Values===============',k,v)
+        sparkBuilder = sparkBuilder.config(k,v)
+    spark = sparkBuilder.getOrCreate()
     sc = spark.sparkContext
     
     job_module = importlib.import_module('jobs.%s' % args.job_name)
 
     start = time.time()
     # TODO: Supply Config to the job as argument.
-    job_module.analyze(spark, sc, **job_args)
+    job_module.analyze(spark, sc,config, **job_args)
     end = time.time()
 
     print("\nExecution of job %s took %s seconds" % (args.job_name, end-start))
